19ab2208bbe6aafe2ac85e2ffb9fe9f154da832e
MD SAIFUL ISLAM <mdsaifulislam38msi@gmail.com>
2026-01-25 20:41:56 +0530

    Initial Commit: Flipkart Clone

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Product = require('./models/productModel');
+const Product = require('../models/productModel');
 
 dotenv.config({ path: 'backend/config/config.env' });
 
